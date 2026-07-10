7e86ab7e2ec5e0f84aa86f15e5285439691838e4
Yisus2299 <jmzm08@gmail.com>
2026-07-10 02:13:34 -0400

    Fix: Enhance global exception handler for IntegrityError with specific duplicated error message

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -15,29 +15,6 @@ origins = [
     "http://localhost:5173"     # Vite standar Port
 ]
 
-
-# we add the Middleware to the app (a middleware is like a door API watcher who checks every single petition before it gets to Routers)
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,       # it allows petitions from the URLs (the origin list)
-    allow_credentials=True,      # it allows the cookies and authentication credentials
-    allow_methods=["*"],         # it allows all HTTP (GET, POST, PUT, DELETE, etc.)
-    allow_headers=["*"],         # it allows all HTTP headers (incluing the Authorization Token)
-)
-
-
-# we connect the teo routers to the main server
-app.include_router(products.router)
-app.include_router(categories.router)
-app.include_router(auth.router)
-app.include_router(orders.router)
-
-
-# we just let the route HOME just for testing
-@app.get("/")
-def home():
-    return{"message": "Welcome to the Store API Test"}
-
 @app.exception_handler(IntegrityError)
 def integrity_exception_handler(request: Request, exc: IntegrityError):
     error_msg = str(exc.orig)
@@ -54,6 +31,29 @@ def integrity_exception_handler(request: Request, exc: IntegrityError):
         status_code=status.HTTP_400_BAD_REQUEST,
         content={"detail":"Invalid Operation: Invalid data Integrity"}
     )
+
+
+# we add the Middleware to the app (a middleware is like a door API watcher who checks every single petition before it gets to Routers)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,       # it allows petitions from the URLs (the origin list)
+    allow_credentials=True,      # it allows the cookies and authentication credentials
+    allow_methods=["*"],         # it allows all HTTP (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],         # it allows all HTTP headers (incluing the Authorization Token)
+)
+
+# we connect the teo routers to the main server
+app.include_router(products.router)
+app.include_router(categories.router)
+app.include_router(auth.router)
+app.include_router(orders.router)
+
+
+# we just let the route HOME just for testing
+@app.get("/")
+def home():
+    return{"message": "Welcome to the Store API Test"}
+
 
         
     
